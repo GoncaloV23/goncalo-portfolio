06dfe972c2b8c0f5201d7521e7ec5da853df44f5
Tuguer <202100296@estudantes.ips.pt>
2025-11-18 00:39:45 +0000

    Changed background color, Fix profile pic bug in mobile

--- docs/scripts/hamburger-menu.js
+++ docs/scripts/hamburger-menu.js
@@ -1,5 +1,3 @@
-// mobile-menu.js
-
 const hamburger = document.querySelector(".hamburger");
 const mobileNav = document.querySelector(".mobile-nav");
 const mobileMenuInner = document.querySelector(".mobile-menu-inner");
@@ -24,15 +22,12 @@ function preventScroll(e) {
   e.preventDefault();
 }
 
-// Only use 'click', it's simpler and works on desktop + mobile
 if (hamburger && mobileNav && mobileMenuInner) {
-  // Open menu
   hamburger.addEventListener("click", (e) => {
     e.stopPropagation();
     toggleMenu();
   });
 
-  // Close menu
   if (mobileClose) {
     mobileClose.addEventListener("click", (e) => {
       e.stopPropagation();
@@ -40,14 +35,12 @@ if (hamburger && mobileNav && mobileMenuInner) {
     });
   }
 
-  // Close menu when clicking a link
   mobileNav.querySelectorAll("a").forEach((link) => {
     link.addEventListener("click", () => {
       toggleMenu();
     });
   });
 
-  // Close menu if clicking outside
   document.addEventListener("click", (e) => {
     if (mobileNav.classList.contains("open") && !mobileMenuInner.contains(e.target) && !hamburger.contains(e.target)) {
       toggleMenu();
